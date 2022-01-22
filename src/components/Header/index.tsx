a5be352c69c92d66f5560e7eeac525c483f1df05
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-01-22 18:52:48 +0100

    ✨ Add Nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import budgie from '../../assets/budgie.png'
+import Nav from 'components/Nav'
 import './index.css'
 
 const Header: React.FC = () => (
     <header className="main-header">
-        <img className="budgie" src={budgie} alt="Budget Budgie!" />
-        <h1>BUDGET BUDGIE</h1>
-        <img className="budgie" src={budgie} alt="Budget Budgie!" />
+        <Nav />
+        <section className='page-title'>
+            <img className="budgie" src={budgie} alt="Budget Budgie!" />
+            <h1>BUDGET BUDGIE</h1>
+            <img className="budgie" src={budgie} alt="Budget Budgie!" />
+        </section>
     </header>
 )
 
