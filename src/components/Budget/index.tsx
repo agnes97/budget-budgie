643dd5fe6a9c5516b9b691da12537e8127a73cd4
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-02-12 14:55:21 +0100

    ✨ Add BudgetContext

--- src/components/Budget/index.tsx
+++ src/components/Budget/index.tsx
@@ -1,30 +1,21 @@
-import { FC, useState, useEffect} from "react";
+import { FC } from "react";
 import "./index.css"
 
-import { subscribeData } from "services/budget";
-import { Data } from '../../services/budget/types'
 import { ArticlesFirstChild } from "./components/ArticlesFirstChild";
 import { Articles } from "./components/Articles";
-import { initialCategories } from "services/budget/categories";
+import { useBudgetData } from "contexts/Budget";
 
 export const Budget: FC = () => {
-  const [budgetData, setBudgetData] = useState<Data[]>(initialCategories)
-
-  useEffect(() => {
-    const unsubscribe = subscribeData("showcase", (budgetData) => setBudgetData(budgetData))
-    return () => unsubscribe()
-  }, [setBudgetData])
+  const { budgetData } = useBudgetData();
 
   return (
-    <>
-      <section>
+    <section>
       {budgetData.map((_, index: number) =>
         /* DISPLAY EITHER FIRST OR ALL OTHER ARTICLES */
         (index === 0)
           ? <ArticlesFirstChild key={index} budgetData={budgetData} index={index} />
           : <Articles key={index} budgetData={budgetData} index={index} />
       )}
-      </section>
-    </>
+    </section>
   )
 }
