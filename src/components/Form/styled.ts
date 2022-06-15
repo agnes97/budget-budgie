b7132b5321d4faa1fbfb323bac0dce4e3689a392
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-06-15 15:15:19 +0200

    ✨ Add formData handling when creating new Budget

---
+++ src/components/Form/styled.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.form`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+
+  & > div.form__children > div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 1.5rem;
+    gap: 0.5rem;
+
+    & > label {
+      color: var(--text-secondary-color);
+      font-weight: bold;
+    }
+
+    & > input {
+      width: 100%;
+      border: none;
+      outline: none;
+      padding: 0.5rem;
+      border-bottom: 0.1rem solid hsl(208, 21%, 88%);
+
+      &::placeholder {
+        color: var(--text-transparent-color);
+      }
+    }
+  }
+
+  & * span.title-after-button-text {
+    padding: 0 0.5rem;
+    text-transform: uppercase;
+  }
+`
