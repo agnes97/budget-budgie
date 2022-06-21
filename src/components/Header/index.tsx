40abde4af7917072cde61212a7d625adb1d548ce
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-06-21 19:56:46 +0200

    ✨ Add budget title animation and alternate display

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,21 +2,25 @@ import type { FC } from 'react'
 
 import { Nav } from 'components/Nav'
 import { useBudgetData } from 'contexts/Budget'
+import { useUser } from 'contexts/User'
 
 import { StyledHeader } from './styled'
 
 import budgie from '../../assets/budgie.png'
 
 export const Header: FC = () => {
+  const { user } = useUser()
   const { budgetInfo } = useBudgetData()
 
   return (
-    <StyledHeader>
+    <StyledHeader animate={!!user}>
       <Nav />
-      <section className="page-title">
-        <img className="budgie" src={budgie} alt="Budget Budgie!" />
-        <h1>{!budgetInfo.title ? 'BUDGET BUDGIE' : budgetInfo.title}</h1>
-        <img className="budgie" src={budgie} alt="Budget Budgie!" />
+      <section className="page-title-container">
+        <div className="page-title">
+          <img className="budgie" src={budgie} alt="Budget Budgie!" />
+          <h1>{!budgetInfo.title ? 'BUDGET BUDGIE' : budgetInfo.title}</h1>
+          <img className="budgie" src={budgie} alt="Budget Budgie!" />
+        </div>
       </section>
       {budgetInfo.description && (
         <section className="description">{budgetInfo.description}</section>
--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,18 +1,71 @@
-import styled from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 
 import { device } from 'themes/variables'
 
-export const StyledHeader = styled.header`
+interface Props {
+  animate: boolean
+}
+
+const displayAnimation = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`
+
+export const StyledHeader = styled.header<Props>`
   display: flex;
   flex-direction: column;
   width: 100%;
+  padding-bottom: 3.5rem;
 
-  & > section.page-title {
+  & > section.page-title-container {
+    padding-top: 4rem;
+  }
+
+  & section > div.page-title::before {
+    ${(props) =>
+      props.animate
+        ? css`
+            animation: ${displayAnimation} 2s 2s forwards;
+            top: -1rem;
+          `
+        : css`
+            display: none;
+          `};
+    opacity: 0;
+    position: absolute;
+    content: 'Budget Budgie';
+    font-size: 1.8rem;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    background-color: var(--text-secondary-color);
+    background-image: linear-gradient(
+      90deg,
+      rgb(148, 197, 125) 0%,
+      rgb(224, 103, 103) 25%,
+      rgb(246, 178, 106) 50%,
+      rgb(176, 206, 237) 75%,
+      rgb(181, 168, 214) 100%
+    );
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+  }
+
+  & section > div.page-title {
+    position: relative;
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-top: 2.5rem;
     place-content: center;
+    z-index: -1;
 
     @media ${device.ipad} {
       flex-direction: row;
@@ -53,6 +106,5 @@ export const StyledHeader = styled.header`
   & > .description {
     color: var(--text-secondary-color);
     text-align: center;
-    padding-bottom: 2.5rem;
   }
 `
