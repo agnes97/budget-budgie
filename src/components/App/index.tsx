f221b8c89d71d8f2449d90ce68dc791ac33a3f30
Jana Chaloupková <janachaloupkova97@gmail.com>
2022-08-01 13:27:15 +0200

    ⬆️ Upgrade react-router-dom to 6.3.0

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,10 @@
 import type { FC } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import { Home } from 'pages/Home'
 
-const Routes: FC = () => (
-  <Switch>
-    <Route path="/" component={Home} />
-  </Switch>
+export const App: FC = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
 )
-
-export const App: FC = () => <Routes />
